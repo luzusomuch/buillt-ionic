97dc32693a4632373c04e614872b64b80264a378
Ledung <dunglh9012@gmail.com>
2015-08-04 14:10:47 +0700

    fix something

--- builder-ionic/www/js/modules/file/file-detail-controller.js
+++ builder-ionic/www/js/modules/file/file-detail-controller.js
@@ -1,54 +1,64 @@
 angular.module('buiiltApp')
 .controller('FileDetailCtrl', function($timeout,API_URL,$scope,file,authService,fileService,$cordovaFileTransfer) {
-    $scope.isInterested = false;
-    $scope.file = file;
-    $scope.totalLike = file.usersInterestedIn.length;
-    $scope.thumbnail = API_URL+'/media/files/'+file._id +'-' + file.title + '.jpg';
-
-    authService.getCurrentUser().$promise.then(function(data){
-        if (_.find(file.usersInterestedIn,{_id: data._id})) {
-            $scope.isInterested = true;
-        }
-        else {
             $scope.isInterested = false;
-        }
-    });
-
-    $scope.likeDocument = function(value) {
-        fileService.interested({'id': value._id, isInterested: $scope.isInterested}).$promise.then(function(data) {
-           $scope.isInterested = !$scope.isInterested;
-           if ($scope.isInterested) {
-                $scope.totalLike = $scope.totalLike +1;
-           }
-           else {
-                $scope.totalLike = $scope.totalLike -1;
-           }
-        });
-    };
-
-    
-
-    $scope.downloadFile = function(value){
-        alert('11111');
-        document.addEventListener('deviceready', function () {
-            alert('22222222222');
-            var url = value.fileUrl;
-            var targetPath = cordova.file.documentsDirectory + value.name + ".jpg";
-            var trustHosts = true
-            var options = {};
-
-            $cordovaFileTransfer.download(url, targetPath, options, trustHosts)
-            .then(function(result) {
-                alert(result);
-            // Success!
-            }, function(err) {
-                alert('error ' + err);
-                // Error
-            }, function (progress) {
-                $timeout(function () {
-                    $scope.downloadProgress = (progress.loaded / progress.total) * 100;
-                })
+            $scope.file = file;
+            $scope.totalLike = file.usersInterestedIn.length;
+            $scope.thumbnail = API_URL+'/media/files/'+file._id +'-' + file.title + '.jpg';
+            
+            authService.getCurrentUser().$promise.then(function(data){
+                                                       if (_.find(file.usersInterestedIn,{_id: data._id})) {
+                                                       $scope.isInterested = true;
+                                                       }
+                                                       else {
+                                                       $scope.isInterested = false;
+                                                       }
+                                                       });
+            
+            $scope.likeDocument = function(value) {
+            fileService.interested({'id': value._id, isInterested: $scope.isInterested}).$promise.then(function(data) {
+                                                                                                       $scope.isInterested = !$scope.isInterested;
+                                                                                                       if ($scope.isInterested) {
+                                                                                                       $scope.totalLike = $scope.totalLike +1;
+                                                                                                       }
+                                                                                                       else {
+                                                                                                       $scope.totalLike = $scope.totalLike -1;
+                                                                                                       }
+                                                                                                       });
+            };
+            
+            
+            
+            $scope.downloadFile = function(value){
+            alert('11111');
+            document.addEventListener('deviceready', function () {
+                                      alert('22222222222');
+                                      var url = value.fileUrl;
+                                      alert(url);
+                                      alert('3333333');
+                                      alert(value.name);
+                                      alert(cordova);
+                                      alert(cordova.file);
+                                      alert(cordova.file.documentsDirectory);
+                                      
+                                      var targetPath = cordova.file.documentsDirectory + value.name;
+                                      alert(targetPath);
+                                      var trustHosts = true;
+                                      var options = {};
+                                      
+                                      alert('444444444');
+                                      alert($cordovaFileTransfer);
+                                      $cordovaFileTransfer.download(url, targetPath, options, trustHosts)
+                                      .then(function(result) {
+                                            alert(result);
+                                            // Success!
+                                            }, function(err) {
+                                            alert('error ' + err);
+                                            // Error
+                                            }, function (progress) {
+                                            $timeout(function () {
+                                                     $scope.downloadProgress = (progress.loaded / progress.total) * 100;
+                                                     })
+                                            });
+                                      }, false);
+            };
             });
-        }, false);
-    };
-});
