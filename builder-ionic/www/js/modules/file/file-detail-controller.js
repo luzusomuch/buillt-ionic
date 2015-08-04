9f3bffaca37da9a1b066b7ca8ca8cf4abeb49880
Ledung <dunglh9012@gmail.com>
2015-08-04 15:21:12 +0700

    aaaa

--- builder-ionic/www/js/modules/file/file-detail-controller.js
+++ builder-ionic/www/js/modules/file/file-detail-controller.js
@@ -26,30 +26,59 @@ angular.module('buiiltApp')
                                                                                                        });
             };
             
+            function saveImageToPhone(url, success, error) {
+            var canvas, context, imageDataUrl, imageData;
+            var img = new Image();
+            img.onload = function() {
+            canvas = document.createElement('canvas');
+            canvas.width = img.width;
+            canvas.height = img.height;
+            context = canvas.getContext('2d');
+            context.drawImage(img, 0, 0);
+            try {
+            imageDataUrl = canvas.toDataURL('image/jpeg', 1.0);
+            imageData = imageDataUrl.replace(/data:image\/jpeg;base64,/, '');
+            cordova.exec(
+                         success,
+                         error,
+                         'Canvas2ImagePlugin',
+                         'saveImageDataToLibrary',
+                         [imageData]
+                         );
+            }
+            catch(e) {
+            error(e.message);
+            }
+            };
+            try {
+            img.src = url;
+            }
+            catch(e) {
+            error(e.message);
+            }
+            }
+            
             
             
             $scope.downloadFile = function(value){
-            alert('11111');
             document.addEventListener('deviceready', function () {
-                                      alert('22222222222');
+                                      
                                       var url = value.fileUrl;
-                                      alert(url);
-                                      alert('3333333');
-                                      alert(value.name);
-                                      alert(cordova);
-                                      alert(cordova.file);
-                                      alert(cordova.file.documentsDirectory);
                                       
                                       var targetPath = cordova.file.documentsDirectory + value.name;
-                                      alert(targetPath);
                                       var trustHosts = true;
                                       var options = {};
-                                      
-                                      alert('444444444');
-                                      alert($cordovaFileTransfer);
                                       $cordovaFileTransfer.download(url, targetPath, options, trustHosts)
                                       .then(function(result) {
-                                            alert(result);
+                                            alert('Success!!');
+                                            var success = function(msg){
+                                            alert(msg);
+                                            };
+                                            
+                                            var error = function(err){
+                                            alert(err);
+                                            };
+                                            saveImageToPhone(targetPath, success, error);
                                             // Success!
                                             }, function(err) {
                                             alert('error ' + err);
