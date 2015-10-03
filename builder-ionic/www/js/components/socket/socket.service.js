a50916448fcadb16b986a9936c10589c79167729
Vinh Nguyen <vinhnq@songphi.com>
2015-10-03 14:44:02 +0700

    [*] change the dev to production

--- builder-ionic/www/js/components/socket/socket.service.js
+++ builder-ionic/www/js/components/socket/socket.service.js
@@ -6,8 +6,8 @@ angular.module('buiiltApp')
     function(socketFactory, authService, API_URL) {
 
       // socket.io now auto-configures its connection when we ommit a connection url
-      var ioSocket = io('http://localhost:9000/', {
-      // var ioSocket = io('http://www.buiilt.com.au/', {
+      // var ioSocket = io('http://localhost:9000/', {
+      var ioSocket = io('http://www.buiilt.com.au/', {
         // Send auth token on connection, you will need to DI the Auth service above
         query: 'token=' + authService.getToken(),
         path: '/socket.io-client'
