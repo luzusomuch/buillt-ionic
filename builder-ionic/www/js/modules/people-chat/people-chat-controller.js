f1754ab9492f6ba84d616a0efd3af3b53176e579
Vinh Nguyen <vinhnq@songphi.com>
2015-11-27 08:43:39 +0700

    [+] add chat, task, document in people package

--- builder-ionic/www/js/modules/people-chat/people-chat-controller.js
+++ builder-ionic/www/js/modules/people-chat/people-chat-controller.js
@@ -4,7 +4,7 @@ angular.module('buiiltApp')
     $scope.team = team;
     $scope.peopleChat = peopleChat;
     getTasksAndFiles(peopleChat);
-    console.log(peopleChat);
+    filterMessages($scope.peopleChat);
 
     $ionicModal.fromTemplateUrl('modalCreateTaskInPeople.html', {
         scope: $scope,
@@ -50,7 +50,6 @@ angular.module('buiiltApp')
                     file.isOwner = true;
                 }
             });
-            console.log($scope.files);
         });
         taskService.getByPackage({id: people.people, type: 'people'}).$promise.then(function(res){
             $scope.tasks = res;
@@ -60,6 +59,21 @@ angular.module('buiiltApp')
                     task.isOwner = true;
                 }
             });
+        });
+    };
+
+    function filterMessages(peopleChat) {
+        peopleChat.orderedMessages = [];
+        for (var i = peopleChat.messages.length -1; i >= 0; i--) {
+            peopleChat.orderedMessages.push(peopleChat.messages[i]);
+        };
+        _.each(peopleChat.orderedMessages, function(message){
+            if (message.user._id == $scope.currentUser._id) {
+                message.owner = true;
+            }
+            else {
+                message.owner = false;
+            }
         });
     };
 
@@ -76,5 +90,66 @@ angular.module('buiiltApp')
                 });
             },500);
         }
+    };
+
+    socket.on('peopleChat:new', function (peopleChat) {
+        if (peopleChat._id == $scope.peopleChat._id) {
+            $scope.peopleChat = peopleChat;
+            filterMessages($scope.peopleChat);
+        }
+    });
+
+    $scope.enterMessage = function ($event) {
+        if ($event.keyCode === 13) {
+            $event.preventDefault();
+            $scope.sendMessage();
+        } else if (($event.keyCode === 32 || $event.keyCode === 8) && $scope.showPopup) {
+            $event.preventDefault();
+            $scope.showPopup = false;
+        } else if ($event.keyCode === 9) {
+            $event.preventDefault();
+            var mentionPeople = _.remove($scope.peopleChat.members, {_id: $scope.currentUser._id});
+            $scope.getMentionValue(mentionPeople);
+        }
+    };
+
+    $scope.message = {};
+
+    $scope.showPopup = false;
+
+    $scope.getMentionValue = function(mention) {
+        console.log(mention);
+        $scope.message.mentions = [];
+        $scope.message.text = $scope.message.text.substring(0, $scope.message.text.length -1);
+        $scope.message.text += (mention.type == 'team') ? mention.name : mention._id.name;  
+        if (mention.type == 'team') {
+            $scope.message.mentions.push(mention._id);
+        } else {
+            $scope.message.mentions.push(mention._id._id);
+        }
+        $scope.showPopup = false;
+        $timeout(function(){ 
+            document.getElementById("textarea1-people-chat").focus();
+        },500);
+    };
+
+    $scope.$watch('message.text', function(newValue, oldValue) {
+        $scope.mentionPeople = [];
+        if (newValue) {
+            if (newValue.slice(-1) == "@") {
+                $scope.showPopup = true;   
+            }
+        }
+    });
+
+    $scope.sendMessage = function() {
+        peopleChatService.sendMessage({id: $scope.peopleChat._id}, $scope.message).$promise.then(function(res) {
+            $scope.peopleChat = res;
+            filterMessages($scope.peopleChat);
+            $scope.message.text = null;
+            // getAllChatMessageNotificationByUserInPeople($scope.selectedChatPeople);
+        }, function(err){
+            console.log(err);
+        });
     };
 });
