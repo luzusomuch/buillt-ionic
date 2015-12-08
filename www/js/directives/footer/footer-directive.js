51cfceb4719ff02e334161d832c12b5f95612b7c
Vinh Nguyen <vinhnq@songphi.com>
2015-12-08 17:10:33 +0700

    remove not needed things

--- www/js/directives/footer/footer-directive.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('buiiltApp')
-        .directive('builtFooter', function() {
-          return {
-            restrict: 'E',
-            templateUrl: 'js/directives/footer/footer.html'
-          };
-        });
