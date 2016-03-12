2ec26e09a2d3dd245bae89a5f052450d7f74beed
Vinh Nguyen <vinhnq@songphi.com>
2016-03-12 15:11:38 +0700

    [*] change for sign out function

--- builder-ionic/www/js/modules/auth/signout/signout-controller.js
+++ builder-ionic/www/js/modules/auth/signout/signout-controller.js
@@ -2,7 +2,7 @@ angular.module('buiiltApp')
   .controller('SignoutCtrl', function(deviceService,$scope, $window, authService, $state, $location) {
     // authService.logout();
     authService.getCurrentUser().$promise.then(function(user){
-        deviceService.removeDevice({id: user._id}).$promise.then(function(){
+        deviceService.removeDevice({id: user._id, deviceplatform: window.deviceplatform}).$promise.then(function(){
             authService.logout();
         });
     });
