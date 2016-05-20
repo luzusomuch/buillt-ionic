5ce77a52efa59a55bc9b885e1a2320ccba564370
Vinh Nguyen <vinhnq@songphi.com>
2016-05-20 13:46:55 +0700

    change to use get API in local

--- builder-ionic/www/js/modules/auth/signout/signout-controller.js
+++ builder-ionic/www/js/modules/auth/signout/signout-controller.js
@@ -2,7 +2,7 @@ angular.module('buiiltApp')
   .controller('SignoutCtrl', function($ionicLoading, deviceService,$scope, userService, $state, authService) {
     $ionicLoading.show();
     // authService.logout();
-    // userService.get({isMobile: true}).$promise.then(function(user) {
+    // authService.getCurrentuser().$promise.then(function(user) {
     // 	deviceService.removeDevice({id: user._id, deviceplatform: window.deviceplatform}).$promise.then(function(){
 	        authService.logout(function() {
 	        	$ionicLoading.hide();
